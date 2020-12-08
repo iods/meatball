42486deecb92873406147608bb9a4479e4883e63
Rye Miller <millerrye17@gmail.com>
2020-12-07 23:45:22 -0700

    #1: Addition of module structure for course.

---
+++ dark/python/data_types.py
@@ -0,0 +1,40 @@
+# Dark Society Python Development
+#
+# The Basics
+# [1] Data Types
+
+# Fundamantal
+print(bin(12))
+print(int('0b1100', 2))
+
+# Integer [int]
+print(12)
+print(type(12))
+
+# Float [float]
+print(3.14)
+print(type(3.14))
+
+# Boolean [bool]
+
+# String [str]
+print('Dark Society')
+print(type('Dark Society'))
+
+# List [list]
+print([1, 2, 3])
+print(type([1, 2, 3]))
+
+# Tuple [tuple]
+print({9.8, 3.14, 2.7})
+print(type({9.8, 3.14, 2.7}))
+
+# Set [set]
+
+# Dictionary [dict]
+print({'input':'output'})
+print(type({'input':'output'}))
+
+# Complex numbers
+print(1 + 1j)
+print(type(1 + 1j))
---
+++ dark/python/main.py
@@ -0,0 +1,4 @@
+# Dark Society Python Development
+import data_types
+
+print('Dark Society Python Development')
