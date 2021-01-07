58c8f93457dfe026751123f2e4a53728562f61d8
Rye Miller <millerrye17@gmail.com>
2021-01-06 19:59:48 -0700

    #2: Update to hello world explaining shebang in file.

---
+++ examples/hello/hello_shebang.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Using a shebang with Python
+
+Using #!/usr/bin/env python3 is the best way to define
+the shebang (i.e. use this as first line of the
+script), since the python binary may be installed somewhere
+else, env will inspect the PATH environment to find the binary.
+
+"""
+
+print("Hello, World!")
