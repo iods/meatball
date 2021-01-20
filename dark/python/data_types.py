a3d8148d91b550ae7a8b88487596dac6fba103ed
Rye Miller <millerrye17@gmail.com>
2021-01-20 13:12:43 -0700

    #2: Removal of completed data types examples for reference.

--- dark/python/data_types.py
+++ dark/python/data_types.py
@@ -7,20 +7,6 @@
 print(bin(12))
 print(int('0b1100', 2))
 
-# Integer [int]
-print(12)
-print(type(12))
-
-# Float [float]
-print(3.14)
-print(type(3.14))
-
-# Boolean [bool]
-
-# String [str]
-print('Dark Society')
-print(type('Dark Society'))
-
 # List [list]
 print([1, 2, 3])
 print(type([1, 2, 3]))
@@ -32,9 +18,5 @@ print(type({9.8, 3.14, 2.7}))
 # Set [set]
 
 # Dictionary [dict]
-print({'input':'output'})
-print(type({'input':'output'}))
-
-# Complex numbers
-print(1 + 1j)
-print(type(1 + 1j))
+print({'input': 'output'})
+print(type({'input': 'output'}))
